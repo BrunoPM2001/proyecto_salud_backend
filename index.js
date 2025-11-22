0b94ee1c74731fc79171c0ac6c5e905c0096c5f1
Bruno P.M. <115752900+BrunoPM2001@users.noreply.github.com>
2025-11-22 01:56:24 -0500

    Retornando alimentos

--- index.js
+++ index.js
@@ -27,6 +27,16 @@ app.get("/ejercicios", async (req, res) => {
   }
 });
 
+app.get("/alimentos", async (req, res) => {
+  try {
+    const [rows] = await pool.query("SELECT * FROM alimentos");
+    res.json(rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Error en consulta a la BD" });
+  }
+});
+
 app.listen(3000, () => {
   console.log("Servidor corriendo en http://localhost:3000");
 });
